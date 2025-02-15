f75843b4d78f4b54657a37fc4118928817a85b92
mohitkumar7004348@gmail.com <mohitkumar7004348@gmail.com>
2025-02-15 11:55:25 +0000

    Created ServerModule1, web.jpg and LICENSE.txt; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,9 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 
 
 class Form1(Form1Template):
@@ -8,3 +12,12 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def outlined_button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    name = self.text_box_1.text
+    CGPA = int(self.text_box_2.text)
+    number = int(self.text_box_3.text)
+    need_friend = self.check_box_1.checked
+    anvil.server.call('submit', name=name, CGPA=CGPA, number=number, need_friend=need_friend)
+    Notification("Your response has been Recorded with Mohit").show()
---
+++ server_code/ServerModule1.py
@@ -0,0 +1,21 @@
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
+
+# This is a server module. It runs on the Anvil server,
+# rather than in the user's browser.
+#
+# To allow anvil.server.call() to call functions here, we mark
+# them with @anvil.server.callable.
+# Here is an example - you can replace it with your own:
+#
+# @anvil.server.callable
+# def say_hello(name):
+#   print("Hello, " + name + "!")
+#   return 42
+#
+@anvil.server.callable
+def submit(name, CGPA, number, need_friend):
+  app_tables.friend.add_row(name=name, CGPA=CGPA, number=number, need_friend=need_friend)
+  
